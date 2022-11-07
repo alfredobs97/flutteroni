36941d14b2d51906687bfe179fc7a867459fce91
Alfredo <alfredobautista1@gmail.com>
2022-11-07 19:56:27 +0100

    inject provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutteroni/providers/pizza_provider.dart';
 import 'package:flutteroni/theme/app_theme.dart';
+import 'package:flutteroni/ui/add_pizza/add_pizza_page.dart';
 import 'package:flutteroni/ui/home/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutteroni',
       theme: AppTheme.theme(),
-      home: const HomePage(),
+      home: ChangeNotifierProvider(
+        create: (_) => PizzaProvider(),
+        child: const AddPizzaPage(),
+      ),
     );
   }
 }
