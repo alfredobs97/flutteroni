c7be59b37e18370e157dca64ceba2f23a16bedf6
Alfredo <alfredobautista1@gmail.com>
2022-11-07 10:25:15 +0100

    home page

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,10 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutteroni/ui/home/widgets/empty_body_widget.dart';
+import 'package:flutteroni/ui/home/widgets/home_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      appBar: HomeAppBar(),
+      body: Center(
+        child: EmptyBodyWidget(),
+      ),
+    );
   }
 }
