4489308b13ee1321a0895b99053f27981b6a48c9
Alfredo <alfredobautista1@gmail.com>
2022-11-07 17:54:25 +0100

    ingredients list widget

---
+++ lib/ui/add_pizza/widgets/ingredients_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutteroni/data/ingredients.dart';
+import 'package:flutteroni/model/ingredient.dart';
+import 'package:flutteroni/ui/add_pizza/widgets/ingredient_tile_widget.dart';
+
+class IngredientsWidget extends StatelessWidget {
+  IngredientsWidget({super.key});
+
+  final List<Ingredient> ingredients = [
+    Mozzarella(),
+    AOVE(),
+    Pomodoro(),
+    Basilico(),
+    GranaPadano(),
+    Burrata(),
+    Prosciutto(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: ingredients.length,
+      itemBuilder: (BuildContext context, int index) => Padding(
+        padding: const EdgeInsets.only(bottom: 16.0),
+        child: IngredientTileWidget(
+          ingredient: ingredients[index],
+          isSelected: false,
+        ),
+      ),
+    );
+  }
+}
