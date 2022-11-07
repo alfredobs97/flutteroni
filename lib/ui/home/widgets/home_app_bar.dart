f8a9fc8b6018723faeca405596bc408521f89ba8
Alfredo <alfredobautista1@gmail.com>
2022-11-07 10:24:52 +0100

    home app bar

--- lib/ui/home/widgets/home_app_bar.dart
+++ lib/ui/home/widgets/home_app_bar.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutteroni/ui/home/widgets/add_pizza_widget.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppBar();
+    return AppBar(
+      backgroundColor: Theme.of(context).canvasColor,
+      elevation: 0,
+      title: Text('Mis pizzas', style: Theme.of(context).textTheme.headline5?.copyWith(fontWeight: FontWeight.bold)),
+      titleSpacing: 32,
+      actions: const [
+        Padding(
+          padding: EdgeInsets.only(top: 8, bottom: 8, right: 32.0),
+          child: AddPizzaWidget(),
+        ),
+      ],
+    );
   }
 
   @override
-  Size get preferredSize => const Size(0, 60);
+  Size get preferredSize => const Size(0, 48);
 }
