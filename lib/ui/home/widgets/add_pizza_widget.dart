ee4339f0767d06e182ac0d1d619e9f40656edee1
Alfredo <alfredobautista1@gmail.com>
2022-11-08 11:52:20 +0100

    navigate to create pizza

--- lib/ui/home/widgets/add_pizza_widget.dart
+++ lib/ui/home/widgets/add_pizza_widget.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutteroni/ui/add_pizza/add_pizza_page.dart';
 
 class AddPizzaWidget extends StatelessWidget {
   const AddPizzaWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 64,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-          color: Theme.of(context).colorScheme.secondary,
-          width: 2
-        )        
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => const AddPizzaPage()),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.add, color: Theme.of(context).colorScheme.secondary),
-          Icon(Icons.local_pizza, color: Theme.of(context).colorScheme.primary),
-        ],
+      child: Container(
+        width: 64,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Theme.of(context).colorScheme.secondary, width: 2)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.add, color: Theme.of(context).colorScheme.secondary),
+            Icon(Icons.local_pizza, color: Theme.of(context).colorScheme.primary),
+          ],
+        ),
       ),
     );
   }
