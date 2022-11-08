e4067496198296ee8b5cad19b254168e0730b296
Alfredo <alfredobautista1@gmail.com>
2022-11-08 11:52:41 +0100

    pizza list widgets

---
+++ lib/ui/home/widgets/pizza_list_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutteroni/model/pizza.dart';
+import 'package:flutteroni/ui/home/widgets/pizza_widget.dart';
+
+class PizzaListWidget extends StatelessWidget {
+  final List<Pizza> pizzas;
+  const PizzaListWidget({required this.pizzas, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: pizzas.length,
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: PizzaWidget(pizza: pizzas[index]),
+      ),
+    );
+  }
+}
---
+++ lib/ui/home/widgets/pizza_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutteroni/model/pizza.dart';
+
+class PizzaWidget extends StatelessWidget {
+  final Pizza pizza;
+
+  const PizzaWidget({required this.pizza, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            height: 70,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16)),
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            child: Center(
+                child: Text(
+              pizza.name,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  ?.copyWith(fontWeight: FontWeight.bold, color: Theme.of(context).canvasColor, fontSize: 16),
+            )),
+          )
+        ],
+      ),
+    );
+  }
+}
