4093b11e9afcffab18bc557aa8f5444ef70bb622
Alfredo <alfredobautista1@gmail.com>
2022-11-08 11:52:49 +0100

    empty state or pizza list

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutteroni/providers/pizza_provider.dart';
 import 'package:flutteroni/ui/home/widgets/empty_body_widget.dart';
 import 'package:flutteroni/ui/home/widgets/home_app_bar.dart';
+import 'package:flutteroni/ui/home/widgets/pizza_list_widget.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: HomeAppBar(),
+    return Scaffold(
+      appBar: const HomeAppBar(),
       body: Center(
-        child: EmptyBodyWidget(),
+        child: Consumer<PizzaProvider>(
+          builder: (context, model, child) =>
+              model.pizzas.isNotEmpty ? PizzaListWidget(pizzas: model.pizzas) : const EmptyBodyWidget(),
+        ),
       ),
     );
   }
